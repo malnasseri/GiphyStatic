fde56eed75ef12c6accf32a73cb83941baa921bd
Mohammed Alnasseri <mohammedalnasseri@Mohammeds-MacBook-Pro.local>
2018-02-20 02:28:14 -0500

    added refresh function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,22 @@
 	
-	var sports = ["Football","Basketball","Hockey","Tennis","Baseball","Volleyball"];
+	var sports = ["footy","rugby","basketball","hockey","tennis","baseball","volleyball","football"];
 	
       function displayGifs(){
-
+          $("#gifs").empty();
       var sport = $(this).attr("data-name");
-      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=dc6zaTOxFJmzC&limit=10";
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=dc6zaTOxFJmzC&limit=12";
 
       $.ajax({
           url: queryURL,
           method: "GET"
         }).done(function(response) {
           console.log(response);
-          for (var i = 0; i < sports.length; i++) {
+          for (var i = 0; i < response.data.length; i++) {
 
-          var gifsDiv = $("<div class='gifsDiv'>");
+          var gifsDiv = $("<div class='gifsDiv col-xs-12 col-sm-6 col-md-4 col-lg-4'>");
 
           var results = response.data;
-          var rating = $("<p>").text("Rating: " + results[i].rating);
-
-            gifsDiv.append(rating);
+         
           
           var gifImage = $("<img>");
 
@@ -28,6 +26,9 @@
             gifImage.attr("data-state", "still"); 
             gifImage.addClass("image");
             gifsDiv.append(gifImage);
+             var rating = $("<p>").text("Rating: " + results[i].rating);
+
+            gifsDiv.append(rating);
               
             $("#gifs").prepend(gifsDiv);
             
@@ -51,16 +52,17 @@
 
   function displayButtons() {
 
-       	$("#sports").html("");
+       	
 
        	for(var i = 0; i < sports.length; i++){
       		
-       		$("#sports").append("<button class=btn btn-sport" + i + "data-name='" +sports[i]+ "'>" + sports[i] + "</button>");
-       		var a = $(".sport");
-          
-           a.attr("data-name", sports[i]);
-           
-           a.text(sports[i]);
+       		// $("#sports").append("<button class='btn btn-sport'" + i + "data-name='" + sports[i] + "'>" + sports[i] + "</button>");
+       		var sportButton = $('<button>') 
+        sportButton.addClass('btn btn-sport'); // Added a class 
+        sportButton.attr('data-name', sports[i]); // Added a data-attribute
+        sportButton.text(sports[i]); // Provided the initial button text
+        $('#sports').append(sportButton); // Added the button to the HTML
+
  	};
 
  };
@@ -69,23 +71,23 @@
 		
 		event.preventDefault();
 		
-		var searchInput = $("#search-input").val().trim();
+		var searchInput = $("#search-input").val().trim().toLowerCase();
 
-
-
-		if(searchInput != "") {
-			sports.push(searchInput);
-		}
+		
+			if(searchInput !== "") {
+      sports.push(searchInput);
+    }
+		
 		$("#search-input").val("");
 		
-
+    $("#sports").empty();
 		displayButtons();
 	});
   $(document).on("click", ".btn", displayGifs);
 	
-  displayButtons();
+  
 
-	
+	displayButtons();
 
 
 
